683e82bacfab5ffde6549bc60a927a7d4e82760e
Chenjia Li <cligrenet@gmail.com>
2022-07-28 16:36:15 +0200

    Create video types, api/post route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,36 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
+// import type { NextPage } from 'next';
+// import Head from 'next/head';
+import axios from 'axios';
+import { Video } from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
 
-const Home: NextPage = () => {
-	return <h1 className="text-3xl font-bold underline">Hello!</h1>;
+interface IProps {
+	videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+	// console.log(videos);
+	return (
+		<div className="flex flex-col gap-10 videos h-full">
+			{videos.length ? (
+				videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+			) : (
+				<NoResults text={'No Videos'} />
+			)}
+		</div>
+	);
+};
+
+// Fetch data at page loading
+export const getServerSideProps = async () => {
+	const response = await axios.get(`http://localhost:3000/api/post`);
+
+	return {
+		props: {
+			videos: response.data,
+		},
+	};
 };
 
 export default Home;
