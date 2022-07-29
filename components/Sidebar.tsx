35bb0f8206412ca7927de8ac074def56910535f6
Chenjia Li <cligrenet@gmail.com>
2022-07-29 11:43:35 +0200

    Remove the login part in sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -37,28 +37,6 @@ const Sidebar = () => {
 							</div>
 						</Link>
 					</div>
-					{!userProfile && (
-						<div className="px-2 py-4 hidden xl:block">
-							<p className="text-gray-400">Log in to like and comment on posts</p>
-							<div className="pr-4">
-								<GoogleLogin
-									clientId=""
-									render={(renderProps) => (
-										<button
-											className="cursor-pointer bg-white text-lg border-[1px] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-black"
-											onClick={renderProps.onClick}
-											disabled={renderProps.disabled}
-										>
-											Log in
-										</button>
-									)}
-									onSuccess={() => {}}
-									onFailure={() => {}}
-									cookiePolicy="single_ost_origin"
-								/>
-							</div>
-						</div>
-					)}
 
 					<Discover />
 					<SuggestedAccounts />
