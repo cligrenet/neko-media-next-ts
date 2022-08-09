0a26174cc5e9c906bf9955ff6f0abf8939fb570b
Chenjia Li <cligrenet@gmail.com>
2022-08-09 15:44:03 +0200

    Finish creating profile page

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -18,6 +18,19 @@ interface IProps {
 
 const Profile = ({ data }: IProps) => {
 	const { user, userVideos, userLikedVideos } = data;
+	const [showUserVideos, setShowUserVideos] = useState(true);
+	const [videosList, setVideosList] = useState<Video[]>([]);
+
+	const videos = showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
+	const liked = !showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
+
+	useEffect(() => {
+		if (showUserVideos) {
+			setVideosList(userVideos);
+		} else {
+			setVideosList(userLikedVideos);
+		}
+	}, [showUserVideos, userVideos, userLikedVideos]);
 
 	return (
 		<div className="w-full">
@@ -32,6 +45,7 @@ const Profile = ({ data }: IProps) => {
 						layout="responsive"
 					/>
 				</div>
+
 				<div className="flex flex-col justify-center">
 					<p className="md:text-2xl tracking-wider flex gap-1 items-center justify-center text-md font-bold text-primary lowercase">
 						{user.userName.replaceAll(' ', '')}
@@ -41,16 +55,40 @@ const Profile = ({ data }: IProps) => {
 				</div>
 			</div>
 
-			{/* TODO */}
+			<div>
+				<div className="flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full">
+					<p
+						className={`text-xl font-semibold cursor-pointer mt-2 ${videos}`}
+						onClick={() => setShowUserVideos(true)}
+					>
+						Videos
+					</p>
+					<p
+						className={`text-xl font-semibold cursor-pointer mt-2 ${liked}`}
+						onClick={() => setShowUserVideos(false)}
+					>
+						Liked
+					</p>
+				</div>
+
+				<div className="flex gap-6 flex-wrap md:justify-start">
+					{videosList.length > 0 ? (
+						videosList.map((post: Video, idx: number) => <VideoCard post={post} key={idx} />)
+					) : (
+						<NoResults text={`No ${showUserVideos ? '' : 'liked'} videos yet`} />
+					)}
+				</div>
+			</div>
 		</div>
 	);
 };
 
+// Data fetching
 export const getServerSideProps = async ({ params: { id } }: { params: { id: string } }) => {
-	const res = await axios.get(`${BASE_URL}/api/profile/${id}`);
+	const { data } = await axios.get(`${BASE_URL}/api/profile/${id}`);
 
 	return {
-		props: { data: res.data },
+		props: { data: data },
 	};
 };
 
